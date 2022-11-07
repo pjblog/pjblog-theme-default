a478a8f86029cbe80a744d332a540c19e34ca447
沈赟杰 <shenyunjie@znrank.com>
2022-11-07 14:30:13 +0800

    update

---
+++ index.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const clientDictionary = path.resolve(__dirname, 'dist', 'ssr', 'client');
+
+module.exports = {
+  static: path.resolve(__dirname, 'dist', 'ssr', 'client'),
+  createMiddleware: async (state) => {
+    const { getAssets } = await import('@codixjs/vite');
+    const render = await import('./dist/ssr/server/server.mjs');
+    const assets = await getAssets(render.default.prefix, 'src/entries/client.tsx', clientDictionary)
+    return (req, res, next) => {
+      req.HTMLAssets = assets;
+      req.HTMLStates = state;
+      render.default.middleware(req, res, next);
+    }
+  }
+}
