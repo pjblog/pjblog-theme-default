5900789130897b6783412409cf419b1c6d8bff8a
沈赟杰 <shenyunjie@znrank.com>
2022-12-17 19:49:01 +0800

    update

--- src/entries/server.tsx
+++ src/entries/server.tsx
@@ -1,6 +1,7 @@
 import { ServerSiderRender } from '@codixjs/server';
 import Html from '../html';
 import createRouters from '../pages';
+import { extractStyle } from '@ant-design/cssinjs';
 
 export default ServerSiderRender({
   prefix: import.meta.env.BASE_URL,
@@ -8,6 +9,7 @@ export default ServerSiderRender({
   routers: createRouters,
   onAllReady(stream, obj) {
     const client = obj.client;
-    stream.write(`<script>window.INITIALIZE_STATE = ${JSON.stringify(client.toJSON())}</script>`);
+    const cache = obj.cache;
+    stream.write(`<script>window.INITIALIZE_STATE = ${JSON.stringify(client.toJSON())}</script>${extractStyle(cache)}`);
   }
 })
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,11 @@ import { Application, HistoryMode, withImport } from '@codixjs/codix';
 import { Client, ClientProvider } from '@codixjs/fetch';
 import { BlogProvider } from '@pjblog/hooks';
 import { BlogError, Layout } from '../components';
+import { createCache, StyleProvider } from '@ant-design/cssinjs';
 
 export default function(app: Application<HistoryMode>) {
   const client = new Client();
+  const cache = createCache();
 
   if (typeof window !== 'undefined' && window.INITIALIZE_STATE) {
     client.initialize(window.INITIALIZE_STATE);
@@ -13,6 +15,9 @@ export default function(app: Application<HistoryMode>) {
   // SSR 数据容器
   // 注入来自服务端数据
   app.use(ClientProvider, { client });
+
+  // antd 样式抽离
+  app.use(StyleProvider, { cache });
   
   // 博客基础数据注入
   app.use(BlogProvider, { 
@@ -74,5 +79,6 @@ export default function(app: Application<HistoryMode>) {
     PROFILE,
     PASSWORD,
     client,
+    cache,
   }
 }
